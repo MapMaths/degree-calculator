983aab4b2ad29e9c58f648b0ba2f4703ee4cfc90
Glitch (hello-webpage) <none>
2020-07-16 11:03:17 +0000

    🏠👹 Checkpoint ./script.js:17680986/3308 ./index.html:17680986/3416 ./404.html:17680986/137

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ function degadd() {
     document.getElementById("addmina") + document.getElementById("addminb");
   var addsec =
     document.getElementById("addseca") + document.getElementById("addsecb");
+  if (addsec >= 60) {
+    addsec -= 60;
+    addmin += 1;
+  }
+  if (addmin >= 60) {
+    addmin -= 60;
+    adddeg += 1;
+  }
+  document.getElementById("result").innerHTML = "";
 }
