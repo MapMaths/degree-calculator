ba84b16a09613d6576cd8665ccb084bb6f390f8f
Glitch (hello-webpage) <none>
2020-07-16 10:52:15 +0000

    🎂🍶 Checkpoint ./script.js:17680986/1063 ./index.html:17680986/12618

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-function degadd(){
-  var adddeg = parseInt(adddega.value)
+function degadd() {
+  var adddeg =
+    document.getElementById("adddega") + document.getElementById("adddegb");
+  var addmin =
+    document.getElementById("addmina") + document.getElementById("addminb");
+  var addsec =
+    document.getElementById("addseca") + document.getElementById("addsecb");
 }
